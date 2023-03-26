5f10853476f83cb2d48c19c3f0473e2dc2080f4c
MRSlou <79364553+MRSlouzk@users.noreply.github.com>
2023-03-26 14:18:41 +0800

    Update index.py

--- code/index.py
+++ code/index.py
@@ -54,9 +54,15 @@ def pool_2(pool_type, rarity):
 def ship_a():
     import os, pathlib
     try:
+        root_l, dirs_l, files_l = [], [], []
         cot = json.load(open("nonebot-plugin-azurlane-assistant-data/azurlane/ship.json", "r", encoding="utf-8"))
+        for root, dirs, files in os.walk("./"):
+            root_l.append(root)
+            dirs_l.append(dirs)
+            files_l.append(files)
+
     except Exception as e:
-        return jsonify({"error": str(e), "path": os.walk(topdown=True)})
+        return jsonify({"error": str(e), "root": root_l, "dirs": dirs_l, "files": files_l})
     return jsonify(cot["data"])
 
 @app.route('/ship/t')
